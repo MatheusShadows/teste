4cf0146e6c8cbe610465816533464febe6d484ec
Matheus Arnaud Pina Baker <matheus_baker@hotmail.com>
2021-06-10 22:00:35 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,9 @@ import Fonts from '../src/components/Fonts.module.css';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import YouTube from 'react-youtube';
-import React from 'react'
+import React from 'react';
+import Rows from '../styles/rrows.module.css';
+
 export default function Home2() {
   return (
     <div>
@@ -15,20 +17,19 @@ export default function Home2() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
        </Head> 
-       <main className={layouts.bgimg}>
-       <div>
+       <div className={layouts.bgimg}>
+         
       <h1 className={Fonts.firstpart}>"Não é sobre aprender
 a programar e sim
 resolver problemas"</h1>
 
-      <p className={Fonts.name}>"-Willys Campos"</p>
+      <h1 className={Fonts.name}>"-Willys Campos"</h1>
       
       <Link href="https://nextjs.org/docs">
         <button className={Fonts.button}>Entre em Contato
           </button>
           </Link>
       </div>
-      </main>
  <div className={layouts.grid}>
    <div className={layouts.card}>
    <h2 className={Fonts.sobrenos}>Sobre Nós</h2>
@@ -38,7 +39,7 @@ resolver problemas"</h1>
   <div> 
     <>
      
-      <Image 
+      <Image className={layouts.cardy} 
         src="/image/sobre-nos-tecnologia.jpg"
         alt="Picture"
         width={450}
@@ -51,12 +52,20 @@ resolver problemas"</h1>
 </div>
  <h2 className={Fonts.aulas}>Aulas Gravadas </h2>
  <p className={Fonts.aulassub}>Assista abaixo as aulas inaugurais</p>
-  <a className={layouts.grid}>
-  <YouTube videoId='TnSXYLR80Tw'></YouTube>
-  <YouTube videoId='j2ecuFWPNZI'></YouTube>
+  <a>
+  <div className={Rows.row}>
+   <div className={Rows.column}>
+    <div className={Rows.card}>
+  <YouTube videoId='TnSXYLR80Tw' className={layouts.cardy}></YouTube>
+  </div>
+    <div className={Rows.card}>
+  <YouTube videoId='j2ecuFWPNZI' className={layouts.cardy}></YouTube>
+    </div>
+   </div>
+  </div>
   </a>
   <div className={layouts.bgmentores}>
-  <h3>MENTORES</h3>
+  <h3 className={Fonts.mentores} >MENTORES</h3>
   </div>
 </div>
   )}
